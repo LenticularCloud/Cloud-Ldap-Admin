5c4c2b3c75cb7cabe9d5516f5de6b8ad7adf847c
TuxCoder <git@o-g.at>
2016-02-15 19:05:32 +0100

    [+] add admin interface for registration

--- src/Cloud/RegistrationBundle/Controller/AdminController.php
+++ src/Cloud/RegistrationBundle/Controller/AdminController.php
@@ -2,10 +2,14 @@
 
 namespace Cloud\RegistrationBundle\Controller;
 
-use Cloud\RegistrationBundle\Form\Type\RegistrationType;
+use Cloud\LdapBundle\Entity\Password;
+use Cloud\RegistrationBundle\Entity\User;
+use Cloud\RegistrationBundle\Form\Type\EditType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/admin")
@@ -14,11 +18,48 @@ class AdminController extends Controller
 {
     /**
      * @Route("/",name="registration_admin_index")
+     * @Template()
      */
     public function indexAction()
     {
-        $users=$this->getDoctrine()->getManager()->getRepository("CloudRegistrationBundle:User")->findAll();
+        $users = $this->getDoctrine()->getManager()->getRepository("CloudRegistrationBundle:User")->findAll();
+        $form = $this->createForm(EditType::class);
 
-        return ['users'=>$users];
+        return ['users' => $users, 'form_edit' => $form->createView()];
+    }
+
+    /**
+     * @Route("/edit/{user}",name="registration_admin_edit")
+     */
+    public function editAction(Request $request, User $user)
+    {
+        $response = new Response();
+        $form=$this->createForm(EditType::class);
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $data=$form->getData();
+            $em=$this->getDoctrine()->getManager();
+            if($data['action'] ===true ) {
+                $userLdap=new \Cloud\LdapBundle\Entity\User($user->getUsername());
+                $password=new Password();
+
+                $password->setHash($user->getPasswordHash());
+                $password->setId('default');
+                $userLdap->addPassword($password);
+
+                $this->get('cloud.ldap.util.usermanipulator')->create($userLdap);
+                $em->remove($user);
+
+            }elseif($data['action'] ===false ) {
+                $em->remove($user);
+            }else {
+                $response->setStatusCode(400);
+                return $response;
+            }
+            $em->flush();
+        }
+
+        return $response;
     }
 }
---
+++ src/Cloud/RegistrationBundle/Form/Type/EditType.php
@@ -0,0 +1,21 @@
+<?php
+namespace Cloud\RegistrationBundle\Form\Type;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('action', ChoiceType::class, [
+            'choices' => [
+                'accept' => true,
+                'deny' => false],
+            'choices_as_values' => true])
+            ->add('submit', SubmitType::class);
+    }
+}
