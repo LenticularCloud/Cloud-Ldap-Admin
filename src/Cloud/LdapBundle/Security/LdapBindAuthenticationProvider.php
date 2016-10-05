32c9805689f4a6977a17bcc8750f65bfd8389b6b
TuxCoder <git@o-g.at>
2016-10-05 13:40:11 +0200

    [+] disallow empty passwords

--- src/Cloud/LdapBundle/Security/LdapBindAuthenticationProvider.php
+++ src/Cloud/LdapBundle/Security/LdapBindAuthenticationProvider.php
@@ -2,6 +2,9 @@
 namespace Cloud\LdapBundle\Security;
 
 use Symfony\Component\Security\Core\Authentication\Provider\LdapBindAuthenticationProvider as BaseLdapBindAuthenticationProvider;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * 
@@ -10,5 +13,16 @@ use Symfony\Component\Security\Core\Authentication\Provider\LdapBindAuthenticati
  */
 class LdapBindAuthenticationProvider extends BaseLdapBindAuthenticationProvider
 {
-    
+    /**
+     * {@inheritdoc}
+     */
+    protected function checkAuthentication(UserInterface $user, UsernamePasswordToken $token)
+    {
+        $password = $token->getCredentials();
+        if($password===null || $password === '') {
+            throw new BadCredentialsException('The presented password is invalid.');
+        }
+
+        return parent::checkAuthentication($user,$token);
+    }
 }
