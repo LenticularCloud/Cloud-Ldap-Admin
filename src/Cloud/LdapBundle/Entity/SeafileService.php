0dc736947d824012128ed4f3093fe5e1c4526d8a
TuxCoder <git@o-g.at>
2018-08-20 18:09:18 +0200

    patch seafile

---
+++ src/Cloud/LdapBundle/Entity/SeafileService.php
@@ -0,0 +1,32 @@
+<?php
+namespace Cloud\LdapBundle\Entity;
+
+use Cloud\LdapBundle\Entity\Ldap\Attribute;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use \InvalidArgumentException;
+use Cloud\LdapBundle\Security\LdapPasswordEncoderInterface;
+use Cloud\LdapBundle\Security\CryptEncoder;
+use Cloud\LdapBundle\Schemas;
+
+class SeafileService extends Service
+{
+
+    protected function serviceEnabled()
+    {
+        parent::serviceEnabled();
+        $email = $this->getUser()->getEmail();
+        $this->$this->setEmail($email);
+    }
+
+
+    public function getEmail()
+    {
+        return $this->getObject(Schemas\CloudService::class)->getMail();
+    }
+
+    public function setEmail($email)
+    {
+        $this->getObject(Schemas\CloudService::class)->setMail($email);
+    }
+}
